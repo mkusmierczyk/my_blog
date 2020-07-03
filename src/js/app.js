1c90ca9ca2f4f424d1be838411896f70fd4b34ea
Mateusz <makusmierczyk@gmail.com>
2020-07-03 16:10:17 +0200

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+
+
+
+const App = () => {
+    return (
+<><h1>asdsad</h1> </>
+)
+}
+ReactDOM.render(<App/>, document.getElementById("app"));
